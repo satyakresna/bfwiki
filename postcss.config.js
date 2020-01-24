d1a1d3df7e9ba13fd1639b242d84794ba9421a49
Satya Kresna <satyakresna6295@gmail.com>
2020-01-24 12:26:22 +0800

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+const cssnano = require('cssnano')({
+  preset: 'default'
+});
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.html',
+    './src/*.html'
+  ],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' 
+    ? [cssnano]
+    : [],
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
+}
