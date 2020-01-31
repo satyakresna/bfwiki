4ed616125543927e823aaf7fdaeb0f241cdbc44a
Satya Kresna <satyakresna6295@gmail.com>
2020-01-31 23:10:01 +0800

    fix: improvement search units

--- src/js/app.js
+++ src/js/app.js
@@ -52,10 +52,16 @@ document.onreadystatechange = function () {
 
           // Search form
           const $form = document.createRange().createContextualFragment(`
-          <form method="GET">
-            <input type="text" name="search" id="unitName">
-            <button type="button" id="searchUnitBtn">Search</button>
-          </form>
+          <form method="GET" class="w-3/4 mx-auto py-2 my-4">
+          <div class="relative">
+            <span>
+              <input class="focus:outline-0 bg-transparent border-b border-gray-500 focus:border-green-500 placeholder-gray-600 py-2 pr-4 pl-10 w-full appearance-none leading-normal" type="text" placeholder="Search the unit" id="unitName">
+            </span>
+            <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+              <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
+            </div>
+          </div>
+        </form>
           `);
           document.querySelector('main').insertBefore($form, $ul);
 
@@ -63,14 +69,16 @@ document.onreadystatechange = function () {
 
           observeUnitsThumbnail();
 
-          document.getElementById('searchUnitBtn').addEventListener('click', (e) => {
+          document.querySelector('form').addEventListener('submit', (e) => {
             e.preventDefault();
             const $unitName = document.getElementById('unitName');
             if ($unitName.value !== '') {
               const unitName = encodeURI($unitName.value);
               page.replace(`${window.location.pathname}?search=${unitName}`, ctx.state);
+            } else {
+              page.replace(`${window.location.pathname}`, ctx.state);
             }
-          })
+          });
         })
       } else {
         closeMenu();
@@ -98,9 +106,15 @@ document.onreadystatechange = function () {
 
           // Search form
           const $form = document.createRange().createContextualFragment(`
-        <form method="GET">
-          <input type="text" name="search" id="unitName">
-          <button type="button" id="searchUnitBtn">Search</button>
+          <form method="GET" class="w-3/4 mx-auto py-2 my-4">
+          <div class="relative">
+            <span>
+              <input class="focus:outline-0 bg-transparent border-b border-gray-500 focus:border-green-500 placeholder-gray-600 py-2 pr-4 pl-10 w-full appearance-none leading-normal" type="text" placeholder="Search the unit" id="unitName">
+            </span>
+            <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+              <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
+            </div>
+          </div>
         </form>
         `);
           document.querySelector('main').insertBefore($form, $ul);
@@ -108,14 +122,16 @@ document.onreadystatechange = function () {
           // Observe units content
           observeUnitsContent();
 
-          document.getElementById('searchUnitBtn').addEventListener('click', (e) => {
+          document.querySelector('form').addEventListener('submit', (e) => {
             e.preventDefault();
             const $unitName = document.getElementById('unitName');
             if ($unitName.value !== '') {
               const unitName = encodeURI($unitName.value);
               page.replace(`${window.location.pathname}?search=${unitName}`, ctx.state);
+            } else {
+              page.replace(`${window.location.pathname}`, ctx.state);
             }
-          })
+          });
         })
       }
     }
