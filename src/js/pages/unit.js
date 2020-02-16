3026d147a2777f8ab58934f1c5fc641771475620
Satya Kresna <satyakresna6295@gmail.com>
2020-02-16 15:08:22 +0800

    feat: display SP recommendation of unit

--- src/js/pages/unit.js
+++ src/js/pages/unit.js
@@ -34,9 +34,43 @@ export function showUnit(ctx) {
 
 function unitTemplate(unit) {
   return `
-  <article class="flex flex-col items-center m-auto">
-    <img src="${unit.artwork}" alt="${unit.name}'s artwork" width="150" height="150" />
+  <article class="flex flex-col items-center m-auto w-full md:w-1/2 p-4 bg-white my-8">
+    <img src="${unit.artwork}" alt="${unit.name}'s artwork" width="300" height="300" />
     <h1>${unit.name}</h1>
     <p>${unit.gender}</p>
+    ${unit.hasOwnProperty('spRecommendation') ? renderSPRecommendation(unit.spRecommendation) : ''}
   </article>`
+}
+
+function renderSPRecommendation(spRecommendation) {
+  const $section = document.createElement('section');
+  $section.setAttribute('class', 'w-full')
+  for (const sp of spRecommendation) {
+    var title, analysis;
+    const fragment = document.createDocumentFragment();
+    const $ul = document.createElement('ul');
+    for (const item of sp) {
+      if (item.hasOwnProperty('title')) {
+        title = item.title;
+      }
+      if (item.hasOwnProperty('analysis')) {
+        analysis = item.analysis;
+      }
+      if (item.hasOwnProperty('cost') && item.hasOwnProperty('option')) {
+        const $li = document.createElement('li');
+        $li.setAttribute('class', 'flex text-sm justify-between items-center my-4');
+        $li.innerHTML = `<span class="w-9/12 inline-block">${item.option}</span> <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-8 h-8 inline-block">${item.cost}</button>`;
+        fragment.appendChild($li);
+      }
+    }
+    $ul.appendChild(fragment);
+    $section.innerHTML += `
+      <div class="my-8">
+        ${ (title !== undefined) ? `<h2 class="mb-4"><strong>${title}</strong></h2>` : ''}
+        ${$ul.outerHTML}
+        ${ (analysis !== undefined) ? `<p class="mt-4 text-xs"><em>Analysis: ${analysis}</em></p>` : ''} 
+      </div>
+    `;
+  }
+  return $section.outerHTML;
 }
