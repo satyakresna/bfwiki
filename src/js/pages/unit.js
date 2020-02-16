6cd6761ddb34d856d4c7ac5f1bdbdb808e7f18a5
Satya Kresna <satyakresna6295@gmail.com>
2020-02-16 09:39:25 +0800

    perf: working with state to cache the detail of unit

--- src/js/pages/unit.js
+++ src/js/pages/unit.js
@@ -1,24 +1,35 @@
 import { requestUnits } from "../utils/request.js";
 
 export function loadUnit(ctx, next) {
-  ctx.state.unitname = ctx.params.unit.split('_').join(' ');
-  next();
+  // check if we have .state.unit already available
+  // this could for example be a cached html fragment.
+  if (ctx.state.unit) {
+    ctx.unit = ctx.state.unit;
+    next();
+    return;
+  }
+
+  // pretend we're querying some database etc
+  setTimeout(function () {
+    // you can assign properties to the context
+    // for use between these functions. The .state
+    // property is what's saved in history.
+    requestUnits().then(data => {
+      for (const unit of data) {
+        if (unit.name === ctx.params.unit.split('_').join(' ')) {
+          ctx.state.unit = unit;
+          ctx.unit = unit;
+          ctx.save();
+          next();
+        }
+      }
+    });
+  }, 300);
 }
 
 export function showUnit(ctx) {
-  requestUnits().then(data => {
-    let selectedUnit;
-    for (const unit of data) {
-      if (unit.name === ctx.state.unitname) {
-        selectedUnit = unit;
-      }
-    }
-    document.title = `${ctx.state.unitname} - ${ctx.title}`;
-    document.querySelector('main').innerHTML = unitTemplate(selectedUnit);
-  })
-    .catch(error => {
-      document.querySelector('main').textContent = 'Not found.';
-    });
+  document.title = `${ctx.unit.name} - ${ctx.title}`;
+  document.querySelector('main').innerHTML = unitTemplate(ctx.unit);
 }
 
 function unitTemplate(unit) {
