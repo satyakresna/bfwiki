3023a064fa8d3cbcfd75653eea66ca15b91905b9
Satya Kresna <satyakresna6295@gmail.com>
2020-02-16 12:35:52 +0800

    chore(purgecss): update content to purge

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     './src/**/*.html',
     './src/*.html',
     './src/js/*.js',
-    './src/js/pages/*.js'
+    './src/js/components/units/*.js'
   ],
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
