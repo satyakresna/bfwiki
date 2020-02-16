07da120349199e081893f995d1d0bb9d80d10f1a
Satya Kresna <satyakresna6295@gmail.com>
2020-02-16 11:35:22 +0800

    style(rename): search input unit name id

--- src/js/pages/units.js
+++ src/js/pages/units.js
@@ -119,7 +119,7 @@ export function renderUnitsContent(units) {
   <form method="GET" class="w-3/4 mx-auto py-2 my-4">
   <div class="relative">
     <span>
-      <input class="focus:outline-0 bg-transparent border-b border-gray-500 focus:border-green-500 placeholder-gray-600 py-2 pr-4 pl-10 w-full appearance-none leading-normal" type="text" placeholder="Search the unit" id="unitName">
+      <input class="focus:outline-0 bg-transparent border-b border-gray-500 focus:border-green-500 placeholder-gray-600 py-2 pr-4 pl-10 w-full appearance-none leading-normal" type="text" placeholder="Search the unit" id="searchUnitName">
     </span>
     <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
       <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
