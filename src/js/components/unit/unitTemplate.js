ee117002c04ef3e622a3eb6999867daad4319c3f
Satya Kresna <satyakresna6295@gmail.com>
2020-02-16 20:38:13 +0800

    chore(unit): align text left and replace gender with element

--- src/js/components/unit/unitTemplate.js
+++ src/js/components/unit/unitTemplate.js
@@ -4,8 +4,10 @@ export default function unitTemplate(unit) {
   return `
   <article class="flex flex-col items-center m-auto w-full md:w-1/2 p-4 bg-white my-8">
     <img src="${unit.artwork}" alt="${unit.name}'s artwork" width="300" height="300" />
-    <h1>${unit.name}</h1>
-    <p>${unit.gender}</p>
+    <div class="text-left">
+      <h1 class="text-lg"><strong>${unit.name}</strong></h1>
+      <p class="text-sm">${unit.element}</p>
+    </div>
     ${unit.hasOwnProperty('spRecommendation') ? renderSPRecommendation(unit.spRecommendation) : ''}
   </article>`
 }
