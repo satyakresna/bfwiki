2ed0428b82b1c1c3af681d99c281de8e2e7169de
Satya Kresna <satyakresna6295@gmail.com>
2020-02-20 21:01:12 +0800

    fix: don't set timeout because it's just pretend

--- src/js/pages/unit.js
+++ src/js/pages/unit.js
@@ -10,28 +10,22 @@ export function loadUnit(ctx, next) {
     return;
   }
 
-  // pretend we're querying some database etc
-  setTimeout(function () {
-    // you can assign properties to the context
-    // for use between these functions. The .state
-    // property is what's saved in history.
-    requestUnits().then(data => {
-      for (const unit of data) {
-        if (unit.name === ctx.params.unit.split('_').join(' ')) {
-          ctx.state.unit = unit;
-          ctx.unit = unit;
-          ctx.save();
-          next();
-        }
+  requestUnits().then(data => {
+    for (const unit of data) {
+      if (unit.name === ctx.params.unit.split('_').join(' ')) {
+        ctx.state.unit = unit;
+        ctx.unit = unit;
+        ctx.save();
+        next();
       }
-    })
-    .catch(error => {
-      const $p = document.createElement('p');
-      $p.setAttribute('class', 'text-center m-auto font-bold');
-      $p.textContent = 'Opps, failed to get detail of unit. Please try again in 3 minutes...';
-      document.querySelector('main').appendChild($p);
-    });
-  }, 300);
+    }
+  })
+  .catch(error => {
+    const $p = document.createElement('p');
+    $p.setAttribute('class', 'text-center m-auto font-bold');
+    $p.textContent = 'Opps, failed to get detail of unit. Please try again in 3 minutes...';
+    document.querySelector('main').appendChild($p);
+  });
 }
 
 export function showUnit(ctx) {
