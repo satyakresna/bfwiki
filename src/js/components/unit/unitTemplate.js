0c83b55cae7bc3b52e4a80fd5122b5abb1af8683
Satya Kresna <satyakresna6295@gmail.com>
2020-03-07 16:14:47 +0800

    fix: make unit name more large

--- src/js/components/unit/unitTemplate.js
+++ src/js/components/unit/unitTemplate.js
@@ -5,7 +5,7 @@ export default function unitTemplate(unit) {
   <article class="flex flex-col items-center m-auto w-full md:w-1/2 p-4 bg-white my-8">
     <img src="${unit.artwork}" alt="${unit.name}'s artwork" width="300" height="300" />
     <div class="text-left">
-      <h1 class="text-lg"><strong>${unit.name}</strong></h1>
+      <h1 class="text-2xl"><strong>${unit.name}</strong></h1>
       <p class="text-sm">${unit.element}</p>
     </div>
     ${unit.hasOwnProperty('spRecommendation') ? renderSPRecommendation(unit.spRecommendation) : ''}
