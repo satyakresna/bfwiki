28cac7717c35aceae333316e7f0556ae0afec415
Satya Kresna <satyakresna6295@gmail.com>
2020-03-07 16:14:25 +0800

    fix: make SP cost size large

--- src/js/components/unit/renderSPRecommendation.js
+++ src/js/components/unit/renderSPRecommendation.js
@@ -18,7 +18,7 @@ export default function renderSPRecommendation(spRecommendation) {
       if (item.hasOwnProperty('cost') && item.hasOwnProperty('option')) {
         const $li = document.createElement('li');
         $li.setAttribute('class', 'flex text-sm justify-between items-center my-4 py-4 border-b');
-        $li.innerHTML = `<span class="w-9/12 inline-block">${item.option}</span> <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-8 h-8 inline-block">${item.cost}</button>`;
+        $li.innerHTML = `<span class="w-9/12 inline-block">${item.option}</span> <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-10 h-10">${item.cost}</button>`;
         fragment.appendChild($li);
       }
     }
