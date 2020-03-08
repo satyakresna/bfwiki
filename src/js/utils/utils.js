afb72e247579698ea420313d1905b03a9eeac409
Satya Kresna <satyakresna6295@gmail.com>
2020-03-08 11:35:05 +0800

    refactor: remove log canonical path

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -16,7 +16,6 @@ export function setTransition() {
 
 export function trackUrl(ctx) {
   if (window.ga !== undefined) {
-    console.log(ctx.canonicalPath);
     ga('set', 'page', ctx.canonicalPath);
     ga('send', 'pageview');
   }
