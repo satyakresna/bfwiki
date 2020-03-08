a84023d595ee13b2432b36dc9eaf3fc89f8868de
Satya Kresna <satyakresna6295@gmail.com>
2020-03-08 10:52:39 +0800

    chore: log to get title and path

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,9 @@ document.onreadystatechange = function () {
     // Call it!
     page();
 
+    console.log(document.title);
+    console.log(window.location.pathname + window.location.search);
+
     document.getElementById('menuToggle').addEventListener('click', function () {
       setTransition();
     });
