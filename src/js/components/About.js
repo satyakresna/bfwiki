d4479f2d480f70cb03d42b22f0eaf3889371fa00
Satya Kresna <satyakresna6295@gmail.com>
2020-04-23 20:46:25 +0800

    feat: dynamic import in about page

---
+++ src/js/components/About.js
@@ -0,0 +1,18 @@
+export default function () {
+    return document.createRange().createContextualFragment(`
+    <article class="m-8">
+      <h1 class="text-3xl mb-4"><strong>About</strong></h1>
+      <p class="mb-4">This website likes a wiki to compile list of omni units and their SP. 
+      <br>
+      The data source taken from <a href="https://github.com/satyakresna/scraping-bravefrontier" target="_blank" class="underline text-blue-700">scraping Brave Frontier Global Fandom site</a>.
+      <br>
+      The source code of this website is open. Please visit to <a href="https://github.com/satyakresna/bfwiki" target="_blank" class="underline text-blue-700">bfwiki's Github repository</a>.
+      </p>
+      <h2 class="text-2xl mb-4"><strong>Courtesy</strong></h2>
+      <ul>
+        <li>Brave Frontier</li>
+        <li>Brave Frontier Global Fandom</li>
+      </ul>
+    </article>
+  `);
+}
--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -8,24 +8,10 @@ export default function (ctx) {
   console.log('ctx title before:', ctx.title);
   document.title = ctx.title = `About - Brave Frontier Wiki`;
   console.log('ctx title after:', ctx.title);
-  document.querySelector('main').textContent = '';
-  const $about = document.createRange().createContextualFragment(`
-    <article class="m-8">
-      <h1 class="text-3xl mb-4"><strong>About</strong></h1>
-      <p class="mb-4">This website likes a wiki to compile list of omni units and their SP. 
-      <br>
-      The data source taken from <a href="https://github.com/satyakresna/scraping-bravefrontier" target="_blank" class="underline text-blue-700">scraping Brave Frontier Global Fandom site</a>.
-      <br>
-      The source code of this website is open. Please visit to <a href="https://github.com/satyakresna/bfwiki" target="_blank" class="underline text-blue-700">bfwiki's Github repository</a>.
-      </p>
-      <h2 class="text-2xl mb-4"><strong>Courtesy</strong></h2>
-      <ul>
-        <li>Brave Frontier</li>
-        <li>Brave Frontier Global Fandom</li>
-      </ul>
-    </article>
-  `)
-  document.querySelector('main').appendChild($about);
+  import('../components/About.js').then(module => {
+    document.querySelector('main').textContent = '';
+    document.querySelector('main').appendChild(module.default());
+  });
 
   trackUrl(ctx);
 }
