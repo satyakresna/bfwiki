e9fa827bee358c862c65f8376feb9f283b596522
Satya Kresna <satyakresna6295@gmail.com>
2020-04-23 08:47:18 +0800

    refactor: dynamic import share unit

--- src/js/pages/unit.js
+++ src/js/pages/unit.js
@@ -33,27 +33,9 @@ export function showUnit(ctx) {
   import("../components/unit/Profile.js").then(({ default: UnitProfile }) => {
     document.querySelector('main').innerHTML = UnitProfile(ctx.unit);
   });
-  const $shareBtn = document.getElementById('shareBtn');
-  $shareBtn.addEventListener('click', (e) => {
-    if (navigator.share) {
-      const title = document.title;
-      const text = ctx.unit.name;
-      const url = document.querySelector('link[rel=canonical]') && document.querySelector('link[rel=canonical]').href || window.location.href;
-      navigator.share({
-        title,
-        text,
-        url
-      })
-      .then(() => {
-        console.log('Successful share');
-      })
-      .catch(error => {
-        console.log('Error sharing', error);
-      })
-    } else {
-      console.log('Not supported, sorry');
-    }
-  })
+  import("../behaviours/unit/share.js").then(({ default: shareUnit }) => {
+    shareUnit();
+  });
 
   trackUrl(ctx);
 }
