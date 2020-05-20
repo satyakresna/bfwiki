a546dd5dcdbe2fc46256333805c97526657a4ebb
Satya Kresna <satyakresna6295@gmail.com>
2020-05-20 08:52:17 +0800

    perf: defer main script

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -6,25 +6,21 @@ import About from "./pages/about.js";
 import openMenu from "./behaviours/openMenu.js";
 import closeMenu from "./behaviours/closeMenu.js";
 
-document.onreadystatechange = function () {
-  if (document.readyState === 'complete') {
-    page('/', Units);
-    page('/about', About);
-    page('/units/:unit', loadUnit, showUnit);
-    page('*', NotFound);
-    // Call it!
-    page();
+page('/', Units);
+page('/about', About);
+page('/units/:unit', loadUnit, showUnit);
+page('*', NotFound);
+// Call it!
+page();
 
-    document.querySelector('button.header__hamburger-btn').addEventListener('click', function () {
-      openMenu();
-    });
+document.querySelector('button.header__hamburger-btn').addEventListener('click', function () {
+  openMenu();
+});
 
-    document.querySelector('button.nav__hide-btn').addEventListener('click', function () {
-      closeMenu();
-    });
+document.querySelector('button.nav__hide-btn').addEventListener('click', function () {
+  closeMenu();
+});
 
-    document.querySelector('div.menu-underlay').addEventListener('click', function () {
-      closeMenu();
-    });
-  }
-}
+document.querySelector('div.menu-underlay').addEventListener('click', function () {
+  closeMenu();
+});
