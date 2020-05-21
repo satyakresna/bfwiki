4da50df47f78df2bb18dea9057eb65ffe8e72d24
Satya Kresna <satyakresna6295@gmail.com>
2020-05-21 22:05:08 +0800

    fix: display list of SP recommendation

--- src/client/js/components/unit/SPRecommendation.js
+++ src/client/js/components/unit/SPRecommendation.js
@@ -2,35 +2,24 @@ export default function (spRecommendation) {
   const $section = document.createElement('section');
   $section.setAttribute('class', 'w-full')
   for (const sp of spRecommendation) {
-    var title, analysis, total;
     const fragment = document.createDocumentFragment();
     const $ul = document.createElement('ul');
-    for (const item of sp) {
-      if (item.hasOwnProperty('title')) {
-        title = item.title;
-      }
-      if (item.hasOwnProperty('analysis')) {
-        analysis = item.analysis;
-      }
-      if (item.hasOwnProperty('total')) {
-        total = item.total;
-      }
-      if (item.hasOwnProperty('cost') && item.hasOwnProperty('option')) {
-        const $li = document.createElement('li');
-        $li.setAttribute('class', 'flex text-sm justify-between items-center my-4 py-4 border-b');
-        $li.innerHTML = `<span class="w-9/12 inline-block">${item.option}</span> <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-10 h-10">${item.cost}</button>`;
-        fragment.appendChild($li);
-      }
+    for (const item of sp.list) {
+      const $li = document.createElement('li');
+      $li.setAttribute('class', 'flex text-sm justify-between items-center my-4 py-4 border-b');
+      $li.innerHTML = `<span class="w-9/12 inline-block">${item.option}</span> <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-10 h-10">${item.cost}</button>`;
+      fragment.appendChild($li);
     }
     $ul.appendChild(fragment);
     $section.innerHTML += `
       <div class="my-16">
         <div class="flex justify-between my-4 py-4">
-          ${ (title !== undefined) ? `<h2 class="text-xl"><strong>${title}</strong></h2>` : ''}
-          ${ (total !== undefined) ? `<button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-10 h-10 text-center"><strong>${total}</strong></button>` : ''}
+        <h2 class="text-xl"><strong>${sp.title}</strong></h2>
+        <button class="border rounded-full border-yellow-600 bg-blue-600 text-white w-10 h-10 text-center"><strong>${sp.total}</strong></button>
         </div>
         ${$ul.outerHTML}
-        ${ (analysis !== undefined) ? `<p class="mt-4 text-sm leading-loose"><strong>Analysis: </strong><em>${analysis}</em></p>` : ''} 
+        <h3 class="mt-4 text-lg leading-loose"><strong>Analysis</strong></h3>
+        <p class="mt-4 text-sm leading-loose">${sp.analysis}</p>
       </div>
     `;
   }
