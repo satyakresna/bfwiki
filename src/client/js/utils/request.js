cddc58cc83ae7fc96b16db77b34d3effedad6081
Satya Kresna <satyakresna6295@gmail.com>
2020-05-22 18:04:46 +0800

    fix: path request units url

--- src/client/js/utils/request.js
+++ src/client/js/utils/request.js
@@ -1,5 +1,5 @@
 export const requestUnits = async () => {
-  const response = await fetch('https://raw.githubusercontent.com/satyakresna/bravefrontier/master/omniunits/data.json');
+  const response = await fetch('https://raw.githubusercontent.com/satyakresna/bravefrontier/master/src/omniunits/data.json');
   const text = await response.text();
   return JSON.parse(text);
 }
