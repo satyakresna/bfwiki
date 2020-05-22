f8e03edacae3c55f3c51e98f09cf230b1914016b
Satya Kresna <satyakresna6295@gmail.com>
2020-05-22 10:07:10 +0800

    fix: main url to fetch omni units

--- src/client/js/utils/request.js
+++ src/client/js/utils/request.js
@@ -1,5 +1,5 @@
 export const requestUnits = async () => {
-  const response = await fetch('https://raw.githubusercontent.com/satyakresna/scraping-bravefrontier/master/data/omniunits.json');
+  const response = await fetch('https://raw.githubusercontent.com/satyakresna/bravefrontier/master/omniunits/data.json');
   const text = await response.text();
   return JSON.parse(text);
 }
