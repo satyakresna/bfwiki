ef0afe1cb01bc9edad8662f80c56336e6ba318dc
Satya Kresna <satyakresna6295@gmail.com>
2020-05-23 08:28:26 +0800

    refactor(units): don't use state to reduce confuse

--- src/client/js/pages/units.js
+++ src/client/js/pages/units.js
@@ -15,22 +15,23 @@ export default function (ctx) {
     const searchName = searchParams.get('name');
     const searchElement = searchParams.get('element');
 
-    if (ctx.state.units) {
+    requestUnits().then(data => {
       let filteredUnits;
+
       if (searchName && searchElement) {
-        filteredUnits = ctx.state.units.filter(item => {
-          if ((item.name.toLowerCase().indexOf(searchName.toLowerCase()) > -1) && item.element === searchElement) {
+        filteredUnits = data.filter(item => {
+          if ((item.name.toLowerCase().indexOf(searchName) > -1) && item.element === searchElement) {
             return item;
           }
         });
       } else if (searchName) {
-        filteredUnits = ctx.state.units.filter(item => {
-          if ((item.name.toLowerCase().indexOf(searchName.toLowerCase()) > -1)) {
+        filteredUnits = data.filter(item => {
+          if ((item.name.toLowerCase().indexOf(searchName) > -1)) {
             return item;
           }
         });
       } else if (searchElement) {
-        filteredUnits = ctx.state.units.filter(item => {
+        filteredUnits = data.filter(item => {
           if (item.element === searchElement) {
             return item;
           }
@@ -47,87 +48,31 @@ export default function (ctx) {
         document.getElementById('searchUnitElement').value = searchElement;
       }
 
-      if (filteredUnits.length > 0) {
-        observeUnitsContent(filteredUnits);
+      observeUnitsContent(filteredUnits);
+
+      searchUnits(ctx);
+    })
+    .catch(error => {
+      const $p = document.createElement('p');
+      $p.setAttribute('class', 'text-center m-auto font-bold');
+      $p.textContent = 'Opps, failed to get omni units. Please try again...';
+      document.querySelector('main').appendChild($p);
+    });
+  } else {
+    requestUnits().then(data => {
+      const units = [];
+      for (const unit of data) {
+        delete unit.spRecommendation;
+        delete unit.cost;
+        units.push(unit);
       }
 
-      searchUnits(ctx);
-    } else {
-      requestUnits().then(data => {
-        let filteredUnits;
+      UnitsContent(units.slice(0, 100));
 
-        if (searchName && searchElement) {
-          filteredUnits = data.filter(item => {
-            if ((item.name.toLowerCase().indexOf(searchName) > -1) && item.element === searchElement) {
-              return item;
-            }
-          });
-        } else if (searchName) {
-          filteredUnits = data.filter(item => {
-            if ((item.name.toLowerCase().indexOf(searchName) > -1)) {
-              return item;
-            }
-          });
-        } else if (searchElement) {
-          filteredUnits = data.filter(item => {
-            if (item.element === searchElement) {
-              return item;
-            }
-          });
-        }
-
-        UnitsContent(filteredUnits);
-
-        if (searchName) {
-          document.getElementById('searchUnitName').value = searchName;
-        }
-  
-        if (searchElement) {
-          document.getElementById('searchUnitElement').value = searchElement;
-        }
-
-        observeUnitsContent(filteredUnits);
-
-        searchUnits(ctx);
-      })
-      .catch(error => {
-        const $p = document.createElement('p');
-        $p.setAttribute('class', 'text-center m-auto font-bold');
-        $p.textContent = 'Opps, failed to get omni units. Please try again...';
-        document.querySelector('main').appendChild($p);
-      });
-    }
-  } else {
-    if (ctx.state.units) {
-      const begin = 0;
-      const end = 100;
-      UnitsContent(ctx.state.units.slice(begin, end));
-
-      // Observe units content
-      observeUnitsContent(ctx.state.units);
+      observeUnitsContent(units);
 
       searchUnits(ctx);
-    } else {
-      requestUnits().then(data => {
-        const units = [];
-        for (const unit of data) {
-          delete unit.spRecommendation;
-          delete unit.cost;
-          units.push(unit);
-        }
-        ctx.state.units = units;
-        ctx.save();
-
-        const begin = 0;
-        const end = 100;
-        UnitsContent(units.slice(begin, end));
-
-        // Observe units content
-        observeUnitsContent(units);
-
-        searchUnits(ctx);
-      })
-    }
+    });
   }
   closeMenu();
   trackUrl(ctx);
