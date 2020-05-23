e21bfd390c9b376b360e2875ff5bcb22c03c860b
Satya Kresna <satyakresna6295@gmail.com>
2020-05-23 08:03:08 +0800

    fix: observe units if units are not empty

--- src/client/js/behaviours/units/observeUnitsContent.js
+++ src/client/js/behaviours/units/observeUnitsContent.js
@@ -2,32 +2,34 @@ import UnitCard from "../../components/unit/Card.js";
 import observeUnitsThumbnail from "./observeUnitsThumbnail.js";
 
 export default function observeUnitsContent(units) {
-  const lastElementChild = document.querySelector('ul#unitList').lastElementChild;
-  const childrenElement = document.querySelector('ul#unitList').children;
+  if (Array.isArray(units) && units.length > 0) {
+    const lastElementChild = document.querySelector('ul#unitList').lastElementChild;
+    const childrenElement = document.querySelector('ul#unitList').children;
 
-  // Observe and do infinite scroll
-  let contentObserver = new IntersectionObserver(function (entries, self) {
-    if (entries[0].isIntersecting) {
-      const begin = (childrenElement.length - 1) + 1;
-      const end = childrenElement.length + 100;
-      if (childrenElement.length < units.length) {
-        const nextUnits = units.slice(begin, end);
-        const fragement = document.createDocumentFragment();
-        for (const unit of nextUnits) {
-          fragement.appendChild(UnitCard(unit));
+    // Observe and do infinite scroll
+    let contentObserver = new IntersectionObserver(function (entries, self) {
+      if (entries[0].isIntersecting) {
+        const begin = (childrenElement.length - 1) + 1;
+        const end = childrenElement.length + 100;
+        if (childrenElement.length < units.length) {
+          const nextUnits = units.slice(begin, end);
+          const fragement = document.createDocumentFragment();
+          for (const unit of nextUnits) {
+            fragement.appendChild(UnitCard(unit));
+          }
+          document.querySelector('ul#unitList').appendChild(fragement);
+          observeUnitsContent(units);
         }
-        document.querySelector('ul#unitList').appendChild(fragement);
-        observeUnitsContent(units);
+        self.unobserve(entries[0].target);
       }
-      self.unobserve(entries[0].target);
-    }
-  }, {
-    root: null, // page as root
-    rootMargin: '0px',
-    threshold: 1.0
-  });
+    }, {
+      root: null, // page as root
+      rootMargin: '0px',
+      threshold: 1.0
+    });
 
-  contentObserver.observe(lastElementChild);
+    contentObserver.observe(lastElementChild);
 
-  observeUnitsThumbnail();
+    observeUnitsThumbnail();
+  }
 }
