5cd620a428b3393845a51c668be5fa63c184b6bd
Satya Kresna <satyakresna6295@gmail.com>
2020-05-24 13:40:07 +0800

    refactor: width of unit card

--- src/client/js/components/unit/Card.js
+++ src/client/js/components/unit/Card.js
@@ -1,8 +1,10 @@
 export default function (unit) {
   return document.createRange().createContextualFragment(`
-  <li class="flex flex-col items-center p-4 m-4 w-1/2 md:w-1/6 bg-white shadow border border-gray-400 rounded">
+  <li class="flex flex-col items-center m-4 w-1/2 md:w-1/6 h-32 bg-white shadow rounded">
+    <div class="flex flex-col items-center m-auto">
       <img data-src="${unit.thumbnail}" width="50" height="50"/>
       <p class="text-sm mt-2"><a href="units/${unit.name.split(' ').join('_')}" class="hover:underline hover:text-blue-700"><strong>${unit.name}</strong></a></p>
+    </div>
   </li>
   `);
 }
