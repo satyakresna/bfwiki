43d94f0d23ed0365b19a2f543855770281bbb3da
Satya Kresna <satyakresna6295@gmail.com>
2020-05-24 22:18:01 +0800

    fix: unit card in responsive mobile

--- src/client/js/components/unit/Card.js
+++ src/client/js/components/unit/Card.js
@@ -1,9 +1,9 @@
 export default function (unit) {
   return document.createRange().createContextualFragment(`
-  <li class="flex flex-col items-center m-4 w-1/2 md:w-1/6 h-32 bg-white shadow rounded">
+  <li class="flex flex-col items-center p-4 md:p-0 m-4 w-1/2 md:w-1/6 h-32 bg-white shadow rounded">
     <div class="flex flex-col items-center m-auto">
       <img data-src="${unit.thumbnail}" width="50" height="50"/>
-      <p class="text-sm mt-2"><a href="units/${unit.name.split(' ').join('_')}" class="hover:underline hover:text-blue-700"><strong>${unit.name}</strong></a></p>
+      <p class="text-sm text-center mt-2"><a href="units/${unit.name.split(' ').join('_')}" class="hover:underline hover:text-blue-700"><strong>${unit.name}</strong></a></p>
     </div>
   </li>
   `);
