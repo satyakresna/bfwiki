2122747c743210c827e41f92ff086d8788879f53
Satya Kresna <satyakresna6295@gmail.com>
2020-05-24 22:01:32 +0800

    perf(fetch): set response as json

--- src/client/js/utils/request.js
+++ src/client/js/utils/request.js
@@ -1,13 +1,13 @@
 export const requestUnits = async (querystring = '') => {
   const URL = 'https://bravefrontier.satyakresna.io/api/omniunits';
   const response = await fetch(`${URL}${querystring ? `?${querystring}` : ''}`);
-  const text = await response.text();
-  return JSON.parse(text);
+  const json = await response.json();
+  return json;
 }
 
 export const requestUnit = async (name) => {
   const URL = `https://bravefrontier.satyakresna.io/api/omniunits/${name}`;
   const response = await fetch(URL);
-  const text = await response.text();
-  return JSON.parse(text);
+  const json = await response.json();
+  return json;
 }
