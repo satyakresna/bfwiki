92a35b243f293896e20dd4d8bc4d8b20c06fc447
Satya Kresna <satyakresna6295@gmail.com>
2020-05-26 20:29:32 +0800

    fix(a11y): set alt in unit thumbnail

--- src/client/js/components/unit/Card.js
+++ src/client/js/components/unit/Card.js
@@ -2,7 +2,7 @@ export default function (unit) {
   return document.createRange().createContextualFragment(`
   <li class="flex flex-col items-center p-4 md:p-0 m-4 w-1/2 md:w-1/6 h-32 bg-white shadow rounded">
     <div class="flex flex-col items-center m-auto">
-      <img data-src="${unit.thumbnail}" width="50" height="50"/>
+      <img data-src="${unit.thumbnail}" width="50" height="50" alt="${unit.name}'s thumbnail" />
       <p class="text-sm text-center mt-2"><a href="units/${unit.name.split(' ').join('_')}" class="hover:underline hover:text-blue-700"><strong>${unit.name}</strong></a></p>
     </div>
   </li>
