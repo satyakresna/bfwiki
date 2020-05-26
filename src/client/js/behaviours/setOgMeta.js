2a6d56a4d1d024e79029a0c7bcf795447125ded6
Satya Kresna <satyakresna6295@gmail.com>
2020-05-26 20:46:03 +0800

    fix(open graph): set standard params

--- src/client/js/behaviours/setOgMeta.js
+++ src/client/js/behaviours/setOgMeta.js
@@ -1,7 +1,5 @@
-export default function (ctx) {
-    let artwork = ctx.state.unit.artwork;
-    artwork = artwork.substring(0, artwork.indexOf('/revision/latest?cb'));
-    document.querySelector('meta[property="og:title"]').setAttribute('content', `${ctx.title}`);
-    document.querySelector('meta[property="og:description"]').setAttribute('content', `SP recommendation for ${ctx.state.unit.name}`);
-    document.querySelector('meta[property="og:image"]').setAttribute('content', `${artwork}`);
+export default function (params) {
+    document.querySelector('meta[property="og:title"]').setAttribute('content', params.title);
+    document.querySelector('meta[property="og:description"]').setAttribute('content', params.description);
+    document.querySelector('meta[property="og:image"]').setAttribute('content', params.image);
 }
