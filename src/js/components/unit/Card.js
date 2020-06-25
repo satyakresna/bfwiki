9c53dd02a850bc6bf152a7865a1531f6fe698347
Satya Kresna <satyakresna6295@gmail.com>
2020-06-25 20:17:46 +0800

    refactor: use @apply to unit card

--- src/js/components/unit/Card.js
+++ src/js/components/unit/Card.js
@@ -1,9 +1,9 @@
 export default function (unit) {
   return document.createRange().createContextualFragment(`
   <li class="unit-card">
-    <div class="flex flex-col items-center m-auto">
-      <img data-src="${unit.thumbnail}" width="50" height="50" alt="${unit.name}'s thumbnail" />      
-      <p class="text-sm text-center mt-2"><a href="units/${unit.name.split(' ').join('_')}" class="hover:underline hover:text-blue-700"><strong>${unit.name}</strong></a></p>
+    <div class="unit-card-container">
+      <img data-src="${unit.thumbnail}" width="50" height="50" alt="${unit.name}'s thumbnail" />
+      <p class="unit-card-name"><a href="units/${unit.name.split(' ').join('_')}"><strong>${unit.name}</strong></a></p>
     </div>
   </li>
   `);
