d4b8326b24cc57f7404155090e57e243a483a26f
Kresna <satyakresna6295@gmail.com>
2020-09-13 09:39:50 +0800

    feat(home): add feedback and join me section

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -8,6 +8,15 @@ export default function () {
           <p>The data take from <a href="https://github.com/satyakresna/bravefrontier" target="_blank" class="underline text-blue-700">unofficial Brave Frontier API that created by myself</a>.</p>
           <p>The source code of this website is available by visit to <a href="https://github.com/satyakresna/bfwiki" target="_blank" class="underline text-blue-700">github.com/satyakresna/bfwiki</a>.</p>
         </div>
+        <h2 class="text-2xl my-4"><strong>Feedback</strong></h2>
+        <div class="leading-loose">
+          <p>If you have any feedback about feature(s) on this website please submit form in <a href="https://github.com/satyakresna/bfwiki/issues" target="_blank" class="underline text-blue-700">github.com/satyakresna/bfwiki/issues</a></p>
+        </div>
+        <h2 class="text-2xl my-4"><strong>Join me</strong></h2>
+        <div class="leading-loose">
+          <p>Join me on Brave Frontier game. My player ID is 5420823754.</p>
+          <img src="../../assets/images/kresna-player-id.jpg" alt="Kresna player ID" />
+        </div>
         <h2 class="text-2xl my-4"><strong>Courtesy</strong></h2>
         <ul class="leading-loose">
           <li>Brave Frontier</li>
