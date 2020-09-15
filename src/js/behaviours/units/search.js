d067798ded7abe13226531e9ea10c94df74e902a
Kresna <satyakresna6295@gmail.com>
2020-09-16 02:55:18 +0800

    refactor(omniunits): search omni units

--- src/js/behaviours/units/search.js
+++ src/js/behaviours/units/search.js
@@ -1,25 +1,38 @@
 import page from "page";
 
+const formData = new FormData();
+
 export default function () {
-  document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    search();
+  document.getElementById('searchUnitName').addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      if (e.target.value !== "") {
+        if (formData.has('name')) {
+          formData.delete('name');
+        }
+        formData.append('name', e.target.value);  
+      } else {
+        formData.delete('name');
+      }
+      e.preventDefault();
+      search(formData); 
+    }
   });
 
   document.getElementById('searchUnitElement').onchange = (e) => {
+    if (e.target.value !== "") {
+      if (formData.has('element')) {
+        formData.delete('element');
+      }
+      formData.append('element', e.target.value);
+    } else {
+      formData.delete('element');
+    }
     e.preventDefault();
-    search();
+    search(formData);
   }
 }
 
-function search () {
-  const formData = new FormData(document.querySelector('form'));
-  if (document.getElementById('searchUnitName').value) {
-    formData.append('name', document.getElementById('searchUnitName').value);  
-  }
-  if (document.getElementById('searchUnitElement').value) {
-    formData.append('element', document.getElementById('searchUnitElement').value);
-  }
+function search (formData) {
   const queryString = new URLSearchParams(formData).toString();
   if (queryString) {
     page.show(`${window.location.pathname}?${queryString}`);
