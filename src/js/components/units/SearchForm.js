8233a7ea43120f897212f597d0f01d5abba60fcb
Kresna <satyakresna6295@gmail.com>
2020-09-18 21:31:51 +0800

    fix: move searchUnitName below of keywords field regarding on choices.js issue

--- src/js/components/units/SearchForm.js
+++ src/js/components/units/SearchForm.js
@@ -2,14 +2,6 @@ export default function () {
   return document.createRange().createContextualFragment(`
   <form method="GET" class="form">
   <div class="relative mb-4">
-    <span>
-      <input class="focus:outline-0 bg-transparent border-b border-gray-500 focus:border-green-500 placeholder-gray-600 py-2 pr-4 pl-10 w-full appearance-none leading-normal" type="text" placeholder="Search the unit" id="searchUnitName">
-    </span>
-    <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
-      <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
-    </div>
-  </div>
-  <div class="relative mb-4">
     <select id="searchUnitElement" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
       <option value="">Element</option>
       <option value="Fire">Fire</option>
@@ -27,6 +19,14 @@ export default function () {
     <select multiple id="searchUnitKeywords" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
       <option value="">Keywords</option>
     </select>
+  </div>
+  <div class="relative mb-4">
+    <span>
+      <input class="focus:outline-0 bg-transparent border-b border-gray-500 focus:border-green-500 placeholder-gray-600 py-2 pr-4 pl-10 w-full appearance-none leading-normal" type="text" placeholder="Search the unit" id="searchUnitName">
+    </span>
+    <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+      <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
+    </div>
   </div>
 </form>
   `);
