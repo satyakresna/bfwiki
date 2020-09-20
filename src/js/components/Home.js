1005cb914da7ef76b8b5b314e0a12e42b7718426
Kresna <satyakresna6295@gmail.com>
2020-09-20 12:15:55 +0800

    feat: add bfwiki logo and update home page

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -4,7 +4,7 @@ export default function () {
       <article class="p-4 mt-16">
         <h1 class="text-3xl my-4"><strong>Brave Frontier Wiki (Unofficial)</strong></h1>
         <div class="leading-loose">
-          <p>This website provides list of omni units and dbbs from Brave Frontier Global.</p>
+          <p>This website provides list of omni units and Dual Brave Burst (DBB) from Brave Frontier Global.</p>
           <p>The data take from <a href="https://github.com/satyakresna/bravefrontier" target="_blank" rel="noopener" class="underline text-blue-700">unofficial Brave Frontier API that created by myself</a>.</p>
           <p>The source code of this website is available by visit to <a href="https://github.com/satyakresna/bfwiki" target="_blank" rel="noopener" class="underline text-blue-700">github.com/satyakresna/bfwiki</a>.</p>
         </div>
